9fb92781939c48078ecf0655ca3b494354f33ca6
Joschka Weniger <wjoschka@gmail.com>
2025-11-28 13:28:31 +0100

    first commit

---
+++ utils/supabase/server.ts
@@ -0,0 +1,44 @@
+import { Database } from '@/types/supabase';
+import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
+
+interface CreateClientOptions {
+  fetchOptions?: RequestInit;
+}
+
+export async function createClient(options: CreateClientOptions = {}) {
+  const cookieStore = await cookies();
+
+  // Create a server's supabase client with newly configured cookie,
+  // which could be used to maintain user's session
+  return createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!,
+    {
+      // ? - Supabase now uses next's custom fetch implementation, so now we can use next's caching with the supabase client
+      global: {
+        fetch: (url, init) => {
+          const mergedInit: RequestInit = { ...init, ...options.fetchOptions };
+
+          return fetch(url, mergedInit);
+        },
+      },
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            );
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    }
+  );
+}
